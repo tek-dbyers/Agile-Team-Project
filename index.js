8d13112fa4bcc71425ccf84fa9be2744c72b4d29
Jacob Wilkins <jacobwilkins501@gmail.com>
2021-02-01 14:01:31 -0600

    Began implementing functionality for displaying Coming Soon button

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+function getProducts() {
+    
+    return [];
+
+}
+let products = getProducts();
+let comingSoon = document.querySelector('#coming-soon');
+if (products === []) {
+    
+}
+
+/*
 function myFunction() {
     document.getElementsByClassName("button").innerHTML = "Item added to cart";
 }
+*/
